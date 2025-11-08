37831c32c09f69bf0d36b3a45cd4fb9f5154887b
ChakuMetal <raulhernandezgarcia1983@gmail.com>
2025-11-08 11:13:01 +0100

    Parametrizar JWT_EXPIRES_IN y añadir .env.example

--- utils/createToken.js
+++ utils/createToken.js
@@ -4,7 +4,7 @@ const createToken = (userId) => {
     return jwt.sign(
         { id: userId }, //id del usuario que queremos guardar en el token
         process.env.JWT_SECRET, // secreto para firmar el token
-        { expiresIn: '1h' } // tiempo de expiración del token
+        { expiresIn: process.env.JWT_EXPIRES_IN || '1h' } // tiempo de expiración del token
     ); // con todo esto se genera la firma del token
 }
 module.exports = { createToken }
